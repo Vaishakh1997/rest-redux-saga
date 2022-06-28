f630640059f4ae5c251c4b28bac5eb2dab9e3930
Vaishakh1997 <vaishakhnetla@gmail.com>
2022-06-28 12:46:26 +0530

    fixed

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCountryDetailRequest } from "../actions/detail";
 import { useNavigate, useParams } from "react-router-dom";
+import ContentLoader from "react-content-loader";
 import Loader from "./Loader";
 import '../assets/css/App.css'
 
@@ -24,7 +25,29 @@ function Detail() {
     return ( 
         <>
         <section className="details">
-        {loading === true? <Loader/> :
+        {loading === true? 
+         <ContentLoader
+         width="100%"
+         height="auto"
+         viewBox="0 0 700 300"
+         backgroundColor="#f5f5f5"
+         foregroundColor="#dbdbdb"
+       >
+         <rect  width="7" height="288" />
+         <rect x="6" y="289" rx="3" ry="3" width="669" height="8" />
+         <rect x="670" y="9" rx="3" ry="3" width="6" height="285" />
+         <rect x="55" y="42" rx="16" ry="16" width="274" height="216" />
+         <rect x="412" y="113" rx="3" ry="3" width="102" height="7" />
+         <rect x="402" y="91" rx="3" ry="3" width="178" height="6" />
+         <rect x="405" y="139" rx="3" ry="3" width="178" height="6" />
+         <rect x="416" y="162" rx="3" ry="3" width="102" height="7" />
+         <rect x="405" y="189" rx="3" ry="3" width="178" height="6" />
+         <rect x="5" y="8" rx="3" ry="3" width="669" height="7" />
+         <rect x="406" y="223" rx="14" ry="14" width="72" height="32" />
+         <rect x="505" y="224" rx="14" ry="14" width="72" height="32" />
+         <rect x="376" y="41" rx="3" ry="3" width="231" height="29" />
+       </ContentLoader>
+        :
             <section className="details" id="details">
                 <button style={{border:'1px solid #D3D3D3'}} type="button" className="back-button" id="back-button" onClick={goBack}>Back</button>
 
@@ -71,6 +94,7 @@ function Detail() {
                 })}
                 
             </section>
+
         }
         </section>
         </>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { Input } from 'reactstrap';
 import { fetchCountriesRequest } from "../actions/home";
 import { useNavigate } from "react-router-dom";
 import Loader from "./Loader";
+import ContentLoader from 'react-content-loader'
 import '../assets/css/App.css'
 
 
@@ -39,7 +40,6 @@ function Home() {
     }
 
     const detailPage = (a) =>{
-        // history.push(`/${a}`);
         navigate(`/${a}`)
     }
 
@@ -62,7 +62,46 @@ function Home() {
                         </Input>
                         </div>
                     </div>
-                    {loading === true? <Loader/> :
+                    {loading === true? 
+                      <ContentLoader viewBox="0 0 1360 900" height="auto" width="100%" >
+                      <rect x="30" y="20" rx="8" ry="8" width="200" height="200" />
+                      <rect x="30" y="250" rx="0" ry="0" width="200" height="18" />
+                      <rect x="30" y="275" rx="0" ry="0" width="120" height="20" />
+                      <rect x="250" y="20" rx="8" ry="8" width="200" height="200" />
+                      <rect x="250" y="250" rx="0" ry="0" width="200" height="18" />
+                      <rect x="250" y="275" rx="0" ry="0" width="120" height="20" />
+                      <rect x="470" y="20" rx="8" ry="8" width="200" height="200" />
+                      <rect x="470" y="250" rx="0" ry="0" width="200" height="18" />
+                      <rect x="470" y="275" rx="0" ry="0" width="120" height="20" />
+                      <rect x="690" y="20" rx="8" ry="8" width="200" height="200" />
+                      <rect x="690" y="250" rx="0" ry="0" width="200" height="18" />
+                      <rect x="690" y="275" rx="0" ry="0" width="120" height="20" />
+                      <rect x="910" y="20" rx="8" ry="8" width="200" height="200" />
+                      <rect x="910" y="250" rx="0" ry="0" width="200" height="18" />
+                      <rect x="910" y="275" rx="0" ry="0" width="120" height="20" />
+                      <rect x="1130" y="20" rx="8" ry="8" width="200" height="200" />
+                      <rect x="1130" y="250" rx="0" ry="0" width="200" height="18" />
+                      <rect x="1130" y="275" rx="0" ry="0" width="120" height="20" />
+                      <rect x="30" y="340" rx="8" ry="8" width="200" height="200" />
+                      <rect x="30" y="570" rx="0" ry="0" width="200" height="18" />
+                      <rect x="30" y="595" rx="0" ry="0" width="120" height="20" />
+                      <rect x="250" y="340" rx="8" ry="8" width="200" height="200" />
+                      <rect x="250" y="570" rx="0" ry="0" width="200" height="18" />
+                      <rect x="250" y="595" rx="0" ry="0" width="120" height="20" />
+                      <rect x="470" y="340" rx="8" ry="8" width="200" height="200" />
+                      <rect x="470" y="570" rx="0" ry="0" width="200" height="18" />
+                      <rect x="470" y="595" rx="0" ry="0" width="120" height="20" />
+                      <rect x="690" y="340" rx="8" ry="8" width="200" height="200" />
+                      <rect x="690" y="570" rx="0" ry="0" width="200" height="18" />
+                      <rect x="690" y="595" rx="0" ry="0" width="120" height="20" />
+                      <rect x="910" y="340" rx="8" ry="8" width="200" height="200" />
+                      <rect x="910" y="570" rx="0" ry="0" width="200" height="18" />
+                      <rect x="910" y="595" rx="0" ry="0" width="120" height="20" />
+                      <rect x="1130" y="340" rx="8" ry="8" width="200" height="200" />
+                      <rect x="1130" y="570" rx="0" ry="0" width="200" height="18" />
+                      <rect x="1130" y="595" rx="0" ry="0" width="120" height="20" />
+                      </ContentLoader>
+                     :
                     <div className="country-layout-list">
 
                         {countries && countries.map(country => {
